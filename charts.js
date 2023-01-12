2782af761ee0afd76c47a804c9de674cec8e1801
albertoaignr <albertoaigner@gmail.com>
2023-01-11 22:56:31 -0500

    Deliverable 2: done

--- charts.js
+++ charts.js
@@ -106,11 +106,33 @@ function buildCharts(sample) {
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot('bar', barData ,barLayout);
     // Deliverable 2: 1. Create the trace for the bubble chart.
+    var bubbleData = [ {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode:'markers',
+      marker: {
+        color: otu_ids,
+        size: sample_values,
+        colorscale: 'Rainbow'
+      }
 
+
+    }
+
+    ];
     // Deliverable 2: 2. Create the layout for the bubble chart.
-
+    var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      hovermode: 'closest',
+      hovertext: otu_labels,
+      hoverinfo: 'text',
+      xaxis: {
+        title: "OTU ID"
+      }
+    };
     // Deliverable 2: 3. Use Plotly to plot the data with the layout.
-    
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
     // Deliverable 3: 4. Create the trace for the gauge chart.
     
     // Deliverable 3: 5. Create the layout for the gauge chart.
